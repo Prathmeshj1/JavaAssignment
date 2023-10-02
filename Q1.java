ca35ce27c0291b6397e2f821f3af92b1dbddb12a
Prathmeshj1 <143994847+Prathmeshj1@users.noreply.github.com>
2023-10-02 19:29:16 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,13 @@
+package javaAssignment;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Name: Prathmesh Jadhav");
+		System.out.println("Hobbies: Playing Football");
+		System.out.println("FavMovie: Jackpot");
+
+	}
+
+}
